7f757467872eed5d908fe712f7a960751f94a477
soroz30 <soroz30@yahoo.pl>
2017-10-11 11:12:22 +0200

    Zadanie 11.4 correction

--- constructors.js
+++ constructors.js
@@ -1,17 +1,16 @@
 function Phone(brand, price, color, camera) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
-	this.camera = camera
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+    this.camera = camera
 }
-
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color +
-				" and the price is " + this.price + ".");
+    console.log("The phone brand is " + this.brand + ", color is " + this.color +
+                " and the price is " + this.price + ".");
 }
 
 Phone.prototype.cameraInfo = function() {
-	console.log("The phone camera resolution is " + this.camera);
+    console.log("The phone camera resolution is " + this.camera);
 }
 
 var iPhone6S = new Phone("Apple", 2250, "silver", "12 Mpx");
