1a2c89445da03162a73bcb470f525102a2490d9f
soroz30 <soroz30@yahoo.pl>
2017-10-11 11:13:08 +0200

    Zadanie 11.4 correction

--- constructors.js
+++ constructors.js
@@ -2,8 +2,9 @@ function Phone(brand, price, color, camera) {
     this.brand = brand;
     this.price = price;
     this.color = color;
-    this.camera = camera
+    this.camera = camera;
 }
+
 Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color +
                 " and the price is " + this.price + ".");
